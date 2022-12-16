4f36093ee16818fb8e318eddf85af6be0db635d9
Zijia Xiong <isxiongzj@gmail.com>
2022-12-16 01:40:09 +0000

    Forcibly overwrite the original script when installing

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 mkdir -p /boot/config/home_root
 echo "Downloading the script..."
 curl -sSL https://raw.githubusercontent.com/RRRRRm/unraid-persistent-home/main/init_home.sh > /boot/config/init_home.sh
+cp -f /boot/config/init_home.sh /usr/local/bin/init_home.sh
 echo "Copying all existing files from /root to /boot/config/home_root ..."
 bash /boot/config/init_home.sh -p
 bash /boot/config/init_home.sh -fl
