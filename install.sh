c8cf09ff323c97dd81c68f27ad4efc6ecf47a7da
Zijia Xiong <isxiongzj@gmail.com>
2022-12-16 01:17:55 +0000

    force overwrite if script exists

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-url="https://raw.githubusercontent.com/RRRRRm/unraid-persistent-home/main/init_home.sh"
 mkdir -p /boot/config/home_root
-curl -sSL $url -o /boot/config/init_home.sh
+curl -sSL https://raw.githubusercontent.com/RRRRRm/unraid-persistent-home/main/init_home.sh > /boot/config/init_home.sh
 bash /boot/config/init_home.sh -p
 bash /boot/config/init_home.sh -fl
-echo "bash /boot/config/init_home.sh -fl" >> /boot/config/go
+grep -qF "init_home.sh" /boot/config/go || echo "bash /boot/config/init_home.sh -fl" >> /boot/config/go
