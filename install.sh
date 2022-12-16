892abfdfcf5d65ed8b39f0433d876ac415784b02
Zijia Xiong <isxiongzj@gmail.com>
2022-12-16 00:50:43 +0000

    Fix bug 2.

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 url="https://raw.githubusercontent.com/RRRRRm/unraid-persistent-home/main/init_home.sh"
 mkdir -p /boot/config/home_root
 curl -sSL $url -o /boot/config/init_home.sh
-chmod +x /boot/config/init_home.sh
-cp /boot/config/init_home.sh /usr/local/bin/init_home.sh
 bash /boot/config/init_home.sh -p
 bash /boot/config/init_home.sh -fl
 echo "bash /boot/config/init_home.sh -fl" >> /boot/config/go
