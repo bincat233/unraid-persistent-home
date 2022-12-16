aa0fe766e28d404eb6412657a0602586a17c7594
Zijia Xiong <isxiongzj@gmail.com>
2022-12-16 01:25:40 +0000

    Add prompts during installation

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 mkdir -p /boot/config/home_root
+echo "Downloading the script..."
 curl -sSL https://raw.githubusercontent.com/RRRRRm/unraid-persistent-home/main/init_home.sh > /boot/config/init_home.sh
+echo "Copying all existing files from /root to /boot/config/home_root ..."
 bash /boot/config/init_home.sh -p
 bash /boot/config/init_home.sh -fl
 grep -qF "init_home.sh" /boot/config/go || echo "bash /boot/config/init_home.sh -fl" >> /boot/config/go
+echo "Done!"
